978c4fbdfae3f79ced957519ff54ff45f983607f
sam.bellman <sam.bellman@isoftbet.com>
2022-03-21 00:01:37 +0000

    mrge

--- src/dungeon/dungeonGenerators/abstract-level-generator.ts
+++ src/dungeon/dungeonGenerators/abstract-level-generator.ts
@@ -3,11 +3,10 @@ import { IDungeonGenArgs } from "../dungeon-generator";
 import { IStairsShell } from "../model/i-level-shell";
 import LevelState from "../model/level-state";
 import { GenLevel } from "./model/GenLevel";
-import { IGenLevel } from "./model/i-gen-level";
 import { ILevelGenerator } from "./model/i-level-generator";
 
 export class AbstractLevelGenerator implements ILevelGenerator{
-    protected genLevel:IGenLevel;
+    protected genLevel:GenLevel;
     protected randomGen:Phaser.Math.RandomDataGenerator;
 
     public createLevel(args:IDungeonGenArgs): void {
@@ -16,12 +15,26 @@ export class AbstractLevelGenerator implements ILevelGenerator{
         this.genLevel = new GenLevel(args.levelShell, new LevelState(), this.randomGen);
 
         this.buildLevel();
+
+        this.addMobs();
+
+        this.decorateLeveL();
+
+        args.callback(this.genLevel.levelState); 
     }
 
     /**
      * override this
      */
     protected buildLevel():void {
+    }
+
+    protected addMobs(): void {
+
+    }
+
+    protected decorateLeveL(): void{
+
     }
 
     protected addStairs(stairsShell:IStairsShell, position:ITinyPoint): void {
