607d28784525ec7ba461bccad4291a5f67054983
sam.bellman <sam.bellman@isoftbet.com>
2022-05-16 08:36:29 +0100

    this time with commits

---
+++ src/game/data/characters/skills.ts
@@ -0,0 +1,29 @@
+export class Skills{
+    public static readonly FIGHTING:string = "fighting";
+    public static readonly SHORT_BLADES:string = "short_blades";
+    public static readonly LONG_BLADES:string = "long_blades";
+    public static readonly AXES:string = "axes";
+    public static readonly MACES_AND_FLAILS:string = "maces_and_flails";
+    public static readonly STAVES:string = "staves";
+    public static readonly ARMOUR:string = "armour";
+    public static readonly DODGING:string = "dodging";
+    public static readonly SHIELDS:string = "shields";
+    public static readonly UNARMED_COMBAT:string = "unarmed_combat";
+    public static readonly CONJURATIONS:string = "conjurations";
+    public static readonly HEXES:string = "hexes";
+    public static readonly SUMMONING:string = "summoning";
+    public static readonly NECROMANCY:string = "necromancy";
+    public static readonly TRANSLOCATIONS:string = "translocations";
+    public static readonly TRANSMUTATIONS:string = "transmutations";
+    public static readonly FIRE_MAGIC:string = "fire_magic";
+    public static readonly ICE_MAGIC:string = "ice_magic";
+    public static readonly AIR_MAGIC:string = "air_magic";
+    public static readonly POISON_MAGIC:string = "poison_magic";
+    public static readonly INVOCATIONS:string = "invocations";
+    public static readonly EVOCATIONS:string = "evocations";
+
+    public static readonly XP:string = "xp";
+    public static readonly HP:string = "hp";
+    public static readonly MP_MOD:string = "mp_mod";
+    public static readonly MR:string = "mr";
+}
