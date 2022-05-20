3985de8c23c80c81c5183f330dd2bae550f4e0ef
ISOFTBET\sam.bellman <sam.bellman@isoftbet.com>
2022-05-20 18:25:33 +0100

    so nothing

--- src/game/model/actors/components/i-species-template.ts
+++ src/game/model/actors/components/i-species-template.ts
@@ -7,11 +7,12 @@ export interface ISpeciesTemplate{
     difficulty:string,
     difficulty_priority:number,
     species_flags:string[],
-    aptitudes:Map<string, number>,
+    aptitudes:{},//Map<string, number>,
     can_swim:boolean,
     str:number,
     int:number,
     dex:number,
     levelup_stat_frequency:number,
-    mutations:{"1":{"MUT_SLOW":1,"MUT_HOP":1,"MUT_DAYSTALKER":1},"13":{"MUT_HOP":1}},"recommended_jobs":["JOB_FIGHTER","JOB_BERSERKER","JOB_WARPER","JOB_ARCANE_MARKSMAN","JOB_SUMMONER","JOB_ICE_ELEMENTALIST"]
+    mutations:{},//Map<number, Map<string, number>>,
+    recommended_jobs:string[]
 }
